d263b173e6956d59c90b2f80db718b4b7038d3ad
Rick Choi <jinhwanlazy@gmail.com>
2023-05-01 16:44:59 +0900

    hacks for IMC2023 considering offline use

--- hloc/extractors/openibl.py
+++ hloc/extractors/openibl.py
@@ -11,8 +11,19 @@ class OpenIBL(BaseModel):
     required_inputs = ['image']
 
     def _init(self, conf):
-        self.net = torch.hub.load(
-            'yxgeee/OpenIBL', conf['model_name'], pretrained=True).eval()
+        try:
+            # first try load from cache directory
+            import os
+            cache_dir = os.path.expanduser('~/.cache/torch/hub/yxgeee_OpenIBL_master')
+            weight_path = os.path.expanduser('~/.cache/torch/hub/checkpoints/vgg16_netvlad.pth')
+            self.net = torch.hub.load( 
+                 cache_dir, conf['model_name'], source='local')
+            self.net.load_state_dict(torch.load(
+                weight_path, map_location=next(self.net.parameters()).device))
+            self.net.eval()
+        except:
+            self.net = torch.hub.load(
+                'yxgeee/OpenIBL', conf['model_name'], pretrained=True).eval()
         mean = [0.48501960784313836, 0.4579568627450961, 0.4076039215686255]
         std = [0.00392156862745098, 0.00392156862745098, 0.00392156862745098]
         self.norm_rgb = tvf.Normalize(mean=mean, std=std)
